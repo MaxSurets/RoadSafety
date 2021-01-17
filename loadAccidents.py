60c9d95acb7ca6715a29ad6e540ab64327dd8928
mvaysiberg <mvays11@gmail.com>
2021-01-17 00:38:16 -0500

    load tables

--- loadAccidents.py
+++ loadAccidents.py
@@ -2,7 +2,20 @@ from road_safety.maps.models import Accidents, Scores
 from csv import reader
 
 with open('accidentsTable.csv', 'r') as read_obj:
-    csv_reader = reader(read_obj)
-    for row in read_obj:
-        print(row)
+    #csv_reader = reader(read_obj)
+    for i, row in enumerate(read_obj,start=1):
+        if start == 1:
+            continue
+        else:
+            entries = row.split(",")
+            Accidents.objects.create(lat=entries[0],long=entries[1],street=entries[3],zip=int(entries[4]))
+
+with open('scoresTable.csv', 'r') as read_obj:
+    for i, row in enumerate(read_obj, start=1):
+        if start == 1:
+            continue
+        else:
+            entries = row.split(",")
+            Scores.objects.create(street=entries[0], zip=int(entries[1]), score=float(entries[2]))
+
 
