9620f5172a1010710ca8ca8beb0c264704421cc5
MaxSurets <maxims2019@gmail.com>
2021-01-17 13:44:08 -0500

    Removed unfinished

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,48 +16,22 @@ function App() {
     <Router>
       <div>
         <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href="#home"><img className="logo" src={logo}/></Navbar.Brand>
+          <Navbar.Brand><img className="logo" src={logo}/></Navbar.Brand>
+          <Navbar.Brand>BumpMap</Navbar.Brand>
           <Nav className="mr-auto">
-            <Nav.Link>
-              <Link to="/">Home</Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/map">Map</Link>
-            </Nav.Link>
-            <Nav.Link>
-              <Link to="/about">About Us</Link>
-            </Nav.Link>
           </Nav>
         </Navbar>
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/map">
-            <Map />
-          </Route>
           <Route path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <Home />
+            <Map />
           </Route>
         </Switch>
       </div>
     </Router>
   )
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
 
 export default App;
